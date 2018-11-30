747f3f7d7437d0e72400521ebd1caefce37ca6f5
Siyu Yao <43591415+ysyyuune@users.noreply.github.com>
2018-12-01 06:44:04 +0800

    Update geometry.c

--- geometry.c
+++ geometry.c
@@ -69,3 +69,7 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->y = ((a->y + b->y) / 2.0 );
 
 }
+
+double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c){
+	return fabs((a->x*(b->y-c->y)+b->x*(c->y-a->y)+c->x*(a->y-b->y))/2);
+}
